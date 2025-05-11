f9eea4df8ea8ba6833afb27c70635b94194d00ff
Charlene (Chay) Makwara <158230758+MakChay@users.noreply.github.com>
2025-05-12 00:38:11 +0200

    deploymdent using render

--- app.py
+++ app.py
@@ -471,6 +471,5 @@ def export_items_pdf():
     )
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Create tables if they do not exist
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
